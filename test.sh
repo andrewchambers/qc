7fe079708c85dc9892fade1c52c2dd7dcca47c83
Andrew Chambers <andrewchamberss@gmail.com>
2016-02-20 17:13:52 +1300

    add index rvals

--- test.sh
+++ test.sh
@@ -7,6 +7,9 @@ do
 	timeout 2s ./qc < $t > $t.ssa
 	timeout 2s ../qbe/lisc/lisc < $t.ssa > $t.s
 	timeout 2s gcc $t.s -o $t.bin
-	timeout 2s $t.bin
+	if ! timeout 2s $t.bin
+	then
+		echo "$t failed"
+	fi
 	echo $t PASS	
 done
