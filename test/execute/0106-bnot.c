4837b539696a639aaff04d852763f97669f7540b
Andrew Chambers <andrewchamberss@gmail.com>
2016-12-28 21:17:16 +1300

    add bnot

---
+++ test/execute/0106-bnot.c
@@ -0,0 +1,22 @@
+
+
+int
+main()
+{
+	int x;
+	long long l;
+	
+	x = 0;
+	l = 0;
+	
+	x = ~x;
+	if (x != 0xffffffff)
+		return 1;
+	
+	l = ~l;
+	if (x != 0xffffffffffffffff)
+		return 1;
+
+	
+	return 0;
+}
